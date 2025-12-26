0f0f4360437eddbfea98134b2a9173e9df4c2a4d
Aarmil M. Sadarani <Aarmil@GISPL.Local>
2025-12-26 18:53:43 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    /*------- Sticky Navigation ------*/ 
+    // $(window).scroll(function() {    
+    //     var scroll = $(window).scrollTop();
+       
+    //     if(scroll >= 100) {
+    //         $(".nav-bar").addClass("afixed");
+    //     }
+    //     else{
+    //          $(".nav-bar").removeClass("afixed");
+    //     }
+    // });
+    
+});
